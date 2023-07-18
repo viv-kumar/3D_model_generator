c3b185c5fd48775806e8a574c03a2c7a469ba238
viv007084 <vivekguptatekari@gamil.com>
2023-07-18 16:40:40 +0530

    new server link added

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -57,7 +57,7 @@ const Customizer = () => {
       setGeneratingImg(true);
 
       const response = await fetch(
-        "https://threed-model-generator.onrender.com/",
+        "https://threed-model-generator.onrender.com/api/v1/dalle",
         {
           method: "POST",
           headers: {
